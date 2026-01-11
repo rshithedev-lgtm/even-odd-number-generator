0b514c700d32e4f99d074646ff767e470b83818e
Thaw Zin Win <rshi.thedev@gmail.com>
2026-01-11 16:23:47 +0700

    Day 1: first Javascript Interaction

---
+++ day-01/script.js
@@ -0,0 +1,6 @@
+const message = document.getElementById("message");
+const button = document.getElementById("changeBtn");
+
+button.addEventListener("click", () => {
+  message.textContent = "JavaScript is working! 🚀";
+});
